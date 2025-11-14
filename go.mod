ac771f860aacf73da68cbcccce34259c6c600020
Michael Baklor <mbaklor@gmail.com>
2025-11-14 14:04:26 +0200

    feat(html): render markdown as html

--- go.mod
+++ go.mod
@@ -3,3 +3,8 @@ module github.com/mbaklor/website
 go 1.24.2
 
 require github.com/go-chi/chi/v5 v5.2.1
+
+require (
+	github.com/gomarkdown/markdown v0.0.0-20250810172220-2e2c11897d1a // indirect
+	github.com/gomarkdown/mdtohtml v0.0.0-20240124153210-d773061d1585 // indirect
+)
--- main.go
+++ main.go
@@ -1,12 +1,18 @@
 package main
 
 import (
+	"fmt"
 	"html/template"
 	"log/slog"
 	"net/http"
 	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/gomarkdown/markdown"
+	"github.com/gomarkdown/markdown/ast"
+	"github.com/gomarkdown/markdown/html"
 )
 
 type TemplateInfo struct {
@@ -57,22 +63,64 @@ func (wa *WebApp) LogRequests(next http.Handler) http.Handler {
 func (wa *WebApp) StaticHandler() {}
 
 func (wa *WebApp) RootRouter(r chi.Router) {
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("templates/base.html", "templates/index.html")
+	r.Get("/*", func(w http.ResponseWriter, r *http.Request) {
+		path := strings.Split(r.PathValue("*"), "/")
+		basePath := path[0]
+		pagePath := path[len(path)-1]
+		file := pagePath + ".md"
+		if path[0] == "" {
+			file = "index.html"
+		}
+		wa.logger.Debug("path information", "page path", pagePath, "file", file)
+		tmpl, err := template.ParseFiles("templates/base.html")
+		if err != nil {
+			wa.logger.Error("error serving page, can't open base path", slog.String("error", err.Error()))
+			http.Error(w, "can't open base path: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		if filepath.Ext(file) == ".md" {
+			err = wa.parseMD(tmpl, file)
+		} else {
+			_, err = tmpl.ParseFiles(filepath.Join("templates", file))
+		}
 		if err != nil {
 			wa.logger.Error("error serving page", slog.String("error", err.Error()))
 			http.Error(w, "can't open page: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
-		tmpl.Execute(w, TemplateInfo{Path: "/"})
+		tmpl.Execute(w, TemplateInfo{Path: "/" + basePath})
 	})
-	r.Get("/about", func(w http.ResponseWriter, r *http.Request) {
-		tmpl, err := template.ParseFiles("templates/base.html", "templates/about.html")
-		if err != nil {
-			wa.logger.Error("error serving page", slog.String("error", err.Error()))
-			http.Error(w, "can't open page: "+err.Error(), http.StatusInternalServerError)
-			return
-		}
-		tmpl.Execute(w, TemplateInfo{Path: "/about"})
-	})
+}
+
+func findHeading(node ast.Node) ast.Node {
+	if heading, ok := node.(*ast.Heading); ok {
+		h := heading.GetChildren()[0].AsLeaf()
+		return h
+	}
+	children := node.GetChildren()
+	if len(children) == 0 {
+		return nil
+	}
+	return findHeading(children[0])
+}
+
+func (wa WebApp) parseMD(tmpl *template.Template, file string) error {
+	wa.logger.Info("parsing markdown file")
+	var f []byte
+	f, err := os.ReadFile(filepath.Join("templates", file))
+	if err != nil {
+		return err
+	}
+	md := markdown.Parse(f, nil)
+	heading := findHeading(md)
+	h1 := heading.(*ast.Leaf).Literal
+	h := markdown.Render(md, html.NewRenderer(html.RendererOptions{}))
+	s := fmt.Sprintf("{{define \"title\"}} Mic's Web - %s {{end}} {{define \"body\"}}\n%s\n{{end}}", h1, h)
+	_, err = tmpl.Parse(string(s))
+	if err != nil {
+		return err
+	}
+	return nil
+
 }
